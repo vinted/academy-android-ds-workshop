31eb8ce03e6b1accf901d09fb636ef252d34a83d
Naglis Zemaitis <naglis.zemaitis@vinted.com>
2023-10-17 13:26:29 +0300

    revert changes, prep env for bloom lib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 pluginManagement {
     repositories {
+        maven("https://nexus.vinted.net/repository/maven-all-repos-group/")
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -8,6 +9,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven("https://nexus.vinted.net/repository/maven-all-repos-group/")
         google()
         mavenCentral()
     }
