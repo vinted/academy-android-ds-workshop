fa3f38a2f104fc8928bf62939f3f6c31c8468846
galisamas <znaglis@gmail.com>
2025-08-26 13:33:53 +0300

    clean code

--- app/src/main/java/com/vinted/ds/workshop/MainActivity.kt
+++ app/src/main/java/com/vinted/ds/workshop/MainActivity.kt
@@ -3,7 +3,6 @@ package com.vinted.ds.workshop
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.vinted.bloom.compose.molecule.list.BloomVerticalList
 import com.vinted.bloom.compose.theme.BloomTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,9 +10,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BloomTheme {
-                BloomVerticalList {
 
-                }
             }
         }
     }
