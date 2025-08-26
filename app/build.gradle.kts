b35023d6e41f4bf1096e06d7303efb689b096ef6
galisamas <znaglis@gmail.com>
2025-08-26 11:39:12 +0300

    update versions and adopt compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "com.vinted.ds.workshop"
-    compileSdk = 33
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.vinted.ds.workshop"
@@ -32,17 +33,25 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
     }
 }
 
 dependencies {
 
-    implementation("com.vinted:android-bloom-marketplace:1.21.0")
-    implementation("com.vinted:android-bloom-assets-marketplace:0.169.0")
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.vinted:android-bloom-marketplace:2.28.0") {
+        exclude(group = "com.vinted", module = "android-bloom-assets-ds")
+    }
+    implementation("com.vinted:android-bloom-assets-marketplace:0.412.0")
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.compose.ui:ui:1.9.0")
+    implementation("androidx.compose.material3:material3:1.3.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
